6b52aa4ea77d46c81e661890834e8dc0c514b21b
Weckyy702 <50154363+Weckyy702@users.noreply.github.com>
2025-11-09 15:03:08 +0100

    Fronted/lib: Use zod for validation

--- doko-hub/src/lib/frontend/fetch.ts
+++ doko-hub/src/lib/frontend/fetch.ts
@@ -1,18 +1,30 @@
 import type { APIRoute } from "$lib/server/routes";
+import { SingleStoreColumnBuilderWithAutoIncrement } from "drizzle-orm/singlestore-core";
+import type z from "zod";
 
-export async function get<T>(route: APIRoute): Promise<T> {
-  return (
-    await fetch(route, {
-      method: "GET",
-    })
-  ).json();
+export async function get<T extends z.ZodType>(
+  route: APIRoute,
+  schema: T,
+  do_fetch?: (route: APIRoute, options: any) => Promise<Response>
+): Promise<z.infer<T>> {
+  do_fetch = do_fetch || fetch;
+  return await do_fetch(route, { method: "GET" })
+    .then((b) => b.json())
+    .then(schema.parse);
 }
 
-export async function post<T>(route: APIRoute, obj: T): Promise<any> {
-  return (
-    await fetch(route, {
-      method: "POST",
-      body: JSON.stringify(obj),
-    })
-  ).json();
+export async function post<T extends z.ZodType>(
+  route: APIRoute,
+  obj: any,
+  response_schema: T,
+  do_fetch?: (route: APIRoute, options: any) => Promise<Response>
+): Promise<z.infer<T>> {
+  do_fetch = do_fetch || fetch;
+
+  return await do_fetch(route, {
+    method: "POST",
+    body: JSON.stringify(obj),
+  })
+    .then((b) => b.json())
+    .then(response_schema.parse);
 }
