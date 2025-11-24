f05775de896bdea62c156bae2e55092a9a1bc976
T4ron_ <brauntaron@gmail.com>
2025-11-24 14:33:28 +0000

    Group creation requires creator UUID an optional nickname

--- doko-hub/src/routes/api/group/+server.ts
+++ doko-hub/src/routes/api/group/+server.ts
@@ -41,9 +41,11 @@ export const GET: RequestHandler = async({ fetch }) => {
 
 export const POST: RequestHandler = async(event) => {
     const bodySchema = z.object({
-        name: Name
+        name: Name,
+        creatorId: UUID,
+        nickname: Name.optional()
     });
-    const { name } = await readValidatedBody(event, bodySchema)
+    const { name, creatorId, nickname } = await readValidatedBody(event, bodySchema)
 
     try {
         const creationObj = {
@@ -56,7 +58,29 @@ export const POST: RequestHandler = async(event) => {
             .values(creationObj)
             .returning()
         
-        return created({ message: 'Created PlayGroup', playGroup: insertedGroup as PlayGroup });
+        await event.fetch(`api/group/${insertedGroup.id}/member`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                playerId: creatorId,
+                ...(nickname ? {nickname: nickname} : {})
+            })
+        });
+        
+        const response = await fetch(`api/group/${insertedGroup.id}/member`);
+        const body = await response.json();
+
+        const group: PlayGroup = {
+            id: insertedGroup.id as UUID,
+            name: insertedGroup.name,
+            createdOn: insertedGroup.createdOn ? new Date(insertedGroup.createdOn) : null,
+            lastPlayedOn: insertedGroup.lastPlayedOn ? new Date(insertedGroup.lastPlayedOn) : null,
+            members: body as PlayGroupMember[]
+        };
+
+        return created({ message: 'Created PlayGroup', playGroup: group });
     } catch(error) {
         // Falls die DB einen Fehler wirft
         return serverError({ message: 'Database error while creating PlayGroup' })
